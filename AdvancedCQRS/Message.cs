8f6cc7b53ecf0b0f413e521716313f4071a2f6a2
Payman Labbaf <payman.labbaf@just-eat.com>
2016-06-01 11:59:56 +0100

    Organise SpotLoss into a folder

--- AdvancedCQRS/Message.cs
+++ AdvancedCQRS/Message.cs
@@ -1,41 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AdvancedCQRS
+﻿namespace AdvancedCQRS
 {
     public abstract class Message{}
 
-    public class PositionAcquired : Message
-    {
-        public int Price { get; set; }
-    }
-
-    public class PriceUpdated : Message
-    {
-        public int Price { get; set; }
-    }
-    public class StopLossHit : Message { }
-
-    public class StopLossPriceUpdated : Message
-    {
-        public int Price { get; set; }
-    }
-
-    public class SendToMeIn : Message
-    {
-        public Message Inner { get; set; }
-        public int DelayInSeconds { get; set; }
-    }
-
-    public class RemoveFrom10SecondWindow : Message
-    {
-        public  int Price { get; set; }
-    }
-    public class RemoveFrom13SecondWindow : Message
-    {
-        public  int Price { get; set; }
-    }
 }
--- AdvancedCQRS/StopLoss/StopLossProcessManagerTests.cs
+++ AdvancedCQRS/StopLoss/StopLossProcessManagerTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AdvancedCQRS.StopLoss;
 using NUnit.Framework;
 
 namespace AdvancedCQRS
